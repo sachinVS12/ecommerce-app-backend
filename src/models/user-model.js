1f846c6068e7cbc09cda7e0f7be519a50aad8011
Sachin V S <vs1sachi12@gmail.com>
2026-06-04 10:36:57 +0530

    user-mdoel

--- src/models/user-model.js
+++ src/models/user-model.js
@@ -1,18 +1,54 @@
+// models/User.js
 const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.model({
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: true,
+    required: true,
+    trim: true,
   },
   email: {
     type: String,
     required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
   },
-  phonenumber: {
+  password: {
     type: String,
     required: true,
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  address: {
+    street: String,
+    city: String,
+    state: String,
+    zipCode: String,
+    country: String,
+  },
+  phone: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
 });
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Compare password method
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
